d80910803d11ea878cf128b5495a75b0814c6de0
JeanKim7 <jean.g.km@gmail.com>
2024-03-04 17:40:12 -0800

    Add quantity and price options

--- w3d1hw.py
+++ w3d1hw.py
@@ -47,58 +47,70 @@ sleep(giraffe, 20)
 class ShoppingCart:
     """class that represents an online shopping cart"""
 
-    def __init__(self, items = []):
-        self.items=items
+    def __init__(self, cart = {}):
+        self.cart= cart
 
-    def add(self, added_item):
-        self.items.append(added_item)
-        print(f"{added_item.title()} has been added to your shopping cart!")
+    def add(self, added_item, quantity, price):
+        self.cart[added_item] = {
+            'quantity': quantity,
+            'price': price,
+            'subtotal': quantity * price,
+            }
+        print(f"\nThe following has been added to your cart:\n"
+              f"{added_item.title()} x {self.cart[added_item]['quantity']}\n"
+              f"subtotal: ${self.cart[added_item]['subtotal']:.2f}\n")
 
-    def remove(self, removed_item):
-        self.items.remove(removed_item)
-        print(f"{removed_item.title()} has been removed from your shopping cart!")
+    def remove_items(self, removed_item):
+        self.cart.pop(removed_item)
+        print(f"\n{removed_item.title()} has been removed from your shopping cart!\n")
 
     def show(self):
-        print(self.items)
+        print(f"\n{self.cart}")
+        total = 0
+        for a in self.cart.values():
+            total += a['subtotal']
+        print(f"\ntotal: ${total:.2f}\n")
 
-    def clear(self):
-        self.items.clear()
+
+    def clear_cart(self):
+        self.cart.clear()
 
 
 user_input=""
 while user_input != "quit":
-    user_input = input("Enter an option below to use your shopping cart!\n"
-                       "Enter one of the following options (Add/Remove/Show/Clear/Quit): ").lower()
+    user_input = input("Enter one of the following options (Add/Remove/Show/Clear/Quit): ").lower()
     
     shopping_cart1 = ShoppingCart()
 
     if user_input == "add":
-        add_input = input("What item would you like to add to your shopping cart? ")
-        shopping_cart1.add(add_input)
+        add_input = input("\nWhat item would you like to add to your shopping cart? ")
+        add_quant = int(input("How many of your item would you like to add? "))
+        add_price = float(input("What is the price of your item? "))
+        shopping_cart1.add(add_input, add_quant, add_price)
     
     elif user_input == "remove":
-        remove_input = input("What item would you like to remove from your shopping cart? ")
-        if remove_input in shopping_cart1.items:
-            shopping_cart1.remove(remove_input)
+        remove_input = input("\nWhat item would you like to remove from your shopping cart? ")
+        if remove_input in shopping_cart1.cart:
+            shopping_cart1.remove_items(remove_input)
         else:
-            print(f"{remove_input} is not in your shoping cart!")
+            print(f"\n{remove_input} is not in your shoping cart!\n")
             continue
 
     elif user_input == "show":
-        if shopping_cart1.items:
-            print(shopping_cart1.items)
+        if shopping_cart1.cart:
+            shopping_cart1.show()
         else: 
-            print("Your shopping cart is empty!")
+            print("\nYour shopping cart is empty!\n")
             continue
 
     elif user_input == "clear":
-        shopping_cart1.clear()
-        print("Your shopping cart has been cleared!")
+        shopping_cart1.clear_cart()
+        print("\nYour shopping cart has been cleared!\n")
         continue
 
     elif user_input == "quit":
         continue
     
     else:
-        print("Please choose one of the correct inputs!")
+        print("\nPlease choose one of the correct inputs!\n")
         continue
